e32a09ae33faeab2a2ffce088c0ff85c17cf8ee6
Jesse-Bett <jessekip21@gmail.com>
2023-09-01 19:59:50 +0300

    feat : added dynamic pin colours, added time value as minutes or hours ago

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { useRef, useEffect, useState } from 'react';
-import './App.css';
-import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
-import axios from 'axios';
+import React, { useRef, useEffect, useState } from "react";
+import "./App.css";
+import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+import axios from "axios";
 
 mapboxgl.accessToken =
-  'pk.eyJ1IjoiaHVibG9zZWt1cm8iLCJhIjoiY2xsem0wdzNoMDAxYzNqczZ3eWkwdnlyNyJ9._f-vlL_y1Zc8wAlWDqL06Q';
+  "pk.eyJ1IjoiaHVibG9zZWt1cm8iLCJhIjoiY2xsem0wdzNoMDAxYzNqczZ3eWkwdnlyNyJ9._f-vlL_y1Zc8wAlWDqL06Q";
 
 function App() {
   const mapContainer = useRef(null);
@@ -15,9 +15,18 @@ function App() {
   const measurementsRef = useRef([]);
   const [selectedMarker, setSelectedMarker] = useState(null);
 
+  const markerColors = {
+    Green: "Green",
+    Yellow: "#F4F432", // a less bright yellow
+    Orange: "Orange",
+    Red: "Red",
+    Purple: "Purple",
+    Maroon: "Maroon",
+  };
+
   useEffect(() => {
-    const DEVICE_ID = '641b3069572090002992a7a1';
-    const TOKEN = '8VYUFBK2T4ZHK623';
+    const DEVICE_ID = "641b3069572090002992a7a1";
+    const TOKEN = "8VYUFBK2T4ZHK623";
     axios
       .get(
         `https://api.airqo.net/api/v2/devices/measurements/airqlouds/${DEVICE_ID}?token=${TOKEN}`
@@ -42,30 +51,54 @@ function App() {
           if (map.current) return;
           map.current = new mapboxgl.Map({
             container: mapContainer.current,
-            style: 'mapbox://styles/mapbox/streets-v12',
+            style: "mapbox://styles/mapbox/streets-v12",
             center: [lng, lat],
             zoom: zoom,
           });
 
           measurementsRef.current.forEach((measurement) => {
-            const marker = new mapboxgl.Marker()
+            const markerColor =
+              markerColors[measurement.aqi_color_name] || "blue"; // Default to blue if color is not found
+            console.log(markerColor);
+            const marker = new mapboxgl.Marker({
+              color: markerColor, // Set the marker's color
+            })
               .setLngLat([
                 measurement.siteDetails.approximate_longitude,
                 measurement.siteDetails.approximate_latitude,
               ])
               .addTo(map.current);
 
+            const measurementTime = new Date(measurement.time);
+            const currentTime = new Date();
+
+            // Calculate the time difference in milliseconds
+            const timeDifference = currentTime - measurementTime;
+
+            // Calculate the time in minutes and hours
+            const minutesAgo = Math.floor(timeDifference / (1000 * 60));
+            const hoursAgo = Math.floor(minutesAgo / 60);
+
+            // Determine whether to display minutes or hours
+            const timeAgo =
+              hoursAgo > 0
+                ? `${hoursAgo} ${hoursAgo === 1 ? "hour" : "hours"} ago`
+                : `${minutesAgo} ${
+                    minutesAgo === 1 ? "minute" : "minutes"
+                  } ago`;
+
             const popup = new mapboxgl.Popup().setHTML(
+              // Set the popup's content
               `
                 <h3>${measurement.siteDetails.formatted_name}</h3>
-                <p>Time: ${measurement.time}</p>
+                <p>Time: ${timeAgo}</p>
                 <p>PM2.5: ${measurement.pm2_5}</p>
                 <p>AQI Category: ${measurement.aqi_category}</p>
-                <p>AQI Color Name: ${measurement.aqi_color_name}</p>
               `
             );
+
             marker.setPopup(popup);
-            marker.getElement().addEventListener('click', () => {
+            marker.getElement().addEventListener("click", () => {
               popup.addTo(map.current);
               if (selectedMarker) {
                 selectedMarker.getPopup().remove();
@@ -76,9 +109,9 @@ function App() {
         }
       })
       .catch((error) => {
-        console.log('Error fetching data', error);
+        console.log("Error fetching data", error);
       });
-  }, [lng, lat, zoom, selectedMarker]);
+  }, [lng, lat, zoom, selectedMarker, markerColors]);
 
   return (
     <div>
