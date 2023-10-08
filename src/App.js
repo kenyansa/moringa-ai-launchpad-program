cc714d2abf6f086da90ae6b0276c3f36204a0cc8
Jesse-Bett <jesse.bett@fasi.health>
2023-10-08 03:42:21 +0300

    add responsiveness for mobile devides and tablets

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import "./App.css";
 import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
 import axios from "axios";
 import Header from "./Header";
+import Legend from "./legend";
 
-mapboxgl.accessToken = process.env.ACCESS_TOKEN;
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiaHVibG9zZWt1cm8iLCJhIjoiY2xsem0wdzNoMDAxYzNqczZ3eWkwdnlyNyJ9._f-vlL_y1Zc8wAlWDqL06Q";
 
 function App() {
   const mapContainer = useRef(null);
@@ -24,16 +26,14 @@ function App() {
       Purple: "#B836F5",
       Maroon: "Maroon",
     };
-    const DEVICE_ID = process.env.DEVICE_ID;
-    const TOKEN = process.env.TOKEN;
+    const DEVICE_ID = "641b3069572090002992a7a1";
+    const TOKEN = "8VYUFBK2T4ZHK623";
     axios
       .get(
         `https://api.airqo.net/api/v2/devices/measurements/airqlouds/${DEVICE_ID}?token=${TOKEN}`
       )
       .then((res) => {
         if (res.data.success) {
-          console.log(res.data);
-
           const measurements = res.data.measurements.map((item) => ({
             device: item.device,
             time: item.time,
@@ -133,70 +133,7 @@ function App() {
       <Header />
       <div ref={mapContainer} className="map-container" />
 
-      <div className="legend-container">
-        <h4 className="legend-title">Air Quality Index Legend</h4>
-        <div className="legend">
-          <div className="legend-item">
-            <span
-              className="legend-color"
-              style={{ backgroundColor: "#4FCA57" }}
-            >
-              0 - 12
-            </span>
-            <span>: Good</span>
-          </div>
-          <div className="legend-item">
-            <span
-              className="legend-color"
-              style={{ backgroundColor: "#F4F432" }}
-            >
-              12.1 - 35.4
-            </span>
-            <span>: Moderate</span>
-          </div>
-          <div className="legend-item">
-            <span
-              className="legend-color"
-              style={{ backgroundColor: "#F59636" }}
-            >
-              35.5 - 55.4
-            </span>
-            <span>: Unhealthy for Sensitive Groups</span>
-          </div>
-          <div className="legend-item">
-            <span
-              className="legend-color"
-              style={{ backgroundColor: "#F53636" }}
-            >
-              55.5 - 150.4
-            </span>
-            <span>: Unhealthy</span>
-          </div>
-          <div className="legend-item">
-            <span
-              className="legend-color"
-              style={{ backgroundColor: "#B836F5" }}
-            >
-              150.5 - 250.4
-            </span>
-            <span>: Very Unhealthy</span>
-          </div>
-          <div className="legend-item">
-            <span
-              className="legend-color"
-              style={{ backgroundColor: "#8C2424" }}
-            >
-              250.5 - 500.4
-            </span>
-            <span>: Hazardous</span>
-          </div>
-        </div>
-        <div className="refresh-time">
-          <span>
-            Last Refreshed : <span id="last-refreshed"></span>{" "}
-          </span>
-        </div>
-      </div>
+      <Legend />
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,46 @@
-import React from 'react';
-import './Header.css';
-import nboImage from './assets/nbo.png';
+import React, { useState, useEffect } from "react";
+import "./Header.css";
+import nboImage from "./assets/nbo.png";
 
 function Header() {
+  const [dateTime, setDateTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setDateTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  function formatDateTime(date) {
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const year = date.getFullYear();
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+
+    return `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
+  }
+
   return (
     <div className="header">
       <img src={nboImage} alt="Logo" className="logo" />
       <h2 className="title">Nairobi Air Quality Index</h2>
-      <h5 className="sponsor">Powered by AirQo</h5>
+      <h2 className="title" id="localetime">
+        {formatDateTime(dateTime)}
+      </h2>
+      <a
+        id="airqo"
+        href="https://www.airqo.net/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <h5 className="sponsor">Powered by AirQo</h5>
+      </a>
     </div>
   );
 }
